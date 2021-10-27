92a42ed0b3f0b90a42f4a8fb9df16f7b08b7cfdd
Marko Sreckovic <markosrecko97@hotmail.rs>
2021-10-27 14:14:27 +0100

    [SponzaSample] Basic lighting

--- playground/sponza/shaders/opaque.hlsl
+++ playground/sponza/shaders/opaque.hlsl
@@ -13,6 +13,7 @@ struct VS_Output
     float4 pos : SV_POSITION;
     float2 uv : TEXCOORD;
     float3 normal : NORMAL;
+    float3 worldPos : WORLD_POS;
 };
 
 SamplerState diffuseSampler : register(s0);
@@ -27,10 +28,37 @@ VS_Output vs_main(VS_Input input)
     output.pos = mul(VP, worldPos);
     output.uv = input.uv;
     output.normal = input.normal;
+    output.worldPos = worldPos;
     return output;
 }
 
 float4 ps_main(VS_Output input) : SV_Target
 {
-    return diffuseTexture.Sample(diffuseSampler, input.uv);
+    // Constants
+    const float3 sunDir = normalize(float3(-0.7, 0.5, -1.2));
+    const float3 sunColor = float3(1.0, 1.0, 1.0);
+    const float3 sunPos = float3(0.0f, 0.0f, 0.0f);
+    const float3 pointLightPos = float3(0.0f, 0.0f, 0.0f);
+    const float3 pointLightColor = float3(1.0f, 1.0f, 1.0f);
+    const float ambientFactor = 0.1;
+
+    const float4 objectColor = diffuseTexture.Sample(diffuseSampler, input.uv);
+    if (objectColor.a < 0.01f) discard;
+
+    const float3 N = normalize(input.normal);
+    const float3 L = normalize(sunPos - input.worldPos);
+    const float3 V = normalize(cameraPosition - input.worldPos);
+    const float3 R = reflect(-V, N);
+
+    float3 ambient = ambientFactor * sunColor;
+
+    float diffuseFactor = max(dot(N, L), 0.0);
+    float3 diffuse = diffuseFactor * sunColor;
+
+    float specularFactor = pow(max(dot(V, R), 0.0), 32);
+    float3 specular = 0.5f * specularFactor * sunColor;
+
+    float3 finalColor = (diffuse + ambient + specular) * objectColor.rgb;
+
+    return float4(finalColor, 1.0);
 }
