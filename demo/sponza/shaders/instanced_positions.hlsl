cc9d6fda5f12d67fe8cdd5b4c5a49e9c594dfbf1
Marko Sreckovic <markosrecko97@hotmail.rs>
2021-11-14 14:25:23 +0100

    [SponzaSample] SV_InstanceID demonstrating

--- demo/sponza/shaders/instanced_positions.hlsl
+++ demo/sponza/shaders/instanced_positions.hlsl
@@ -8,7 +8,6 @@ struct VS_Input
 {
     float3 position : POSITION;
     float3 positionOffset : I_POSITION_OFFSET; // I_ prefix will automatically set this attribute as per instance
-    // TODO: Add SV_InstanceID
 };
 
 struct VS_Output
@@ -30,17 +29,14 @@ VS_Output vs_main(VS_Input input, uint instanceID : SV_InstanceID)
     VS_Output output;
     output.pos = mul(MVP, worldPos);
 
-//#ifdef USE_INSTANCE_ID
-//    output.color = float4(input.instanceID / 255, (255 - input.instanceID) / 255, input.instanceID * 2 / 255, 1.0);
-//#else
 #ifdef USE_MODEL
     output.color = float4(1.0, 0.0, 1.0, 0.5);
 #else
-    output.color = float4(0.0, 1.0, 1.0, 1.0);
     output.color.r = instanceID / 100.0f;
     output.color.g = (2 * instanceID / 3) / 67.0f;
+    output.color.b = 1.0 - (instanceID / 100.0f);
+    output.color.a = 1.0;
 #endif
-//#endif 
     return output;
 }
 
